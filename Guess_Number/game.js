7ef545f58b3b0364de2abbf2aaa2618346e24951
hakan <tasbeyhakan@gmail.com>
2023-11-09 04:13:34 +0300

    second commit

--- Guess_Number/game.js
+++ Guess_Number/game.js
@@ -23,6 +23,61 @@ document.querySelector(".check").addEventListener("click",()=>{
         mesaj.textContent = "Tebrikler Bildiniz👏"
         document.querySelector("body").style.backgroundColor = "green"
         document.querySelector(".number").textContent = rastgeleSayi
+
+    //? Top Score Kontrolü
+    if(skor > enYuksekSkor){
+        enYuksekSkor = skor
+        document.querySelector(".top-score").textContent = enYuksekSkor
     }
 
+    //! Tahmin Yanlışsa
+    }else{
+     //? Skor 1'den büyük olduğu sürece tahmin hakkım var
+
+     if(skor>1){
+        skor-- ;
+        document.querySelector(".score").textContent = skor
+
+        tahmin < rastgeleSayi 
+        ? mesaj.textContent = "Arttir👆" 
+        : mesaj.textContent = "Azalt👇"
+
+     }else{
+        mesaj.textContent = "Oyunu Kaybettiniz 🥺"
+        document.querySelector(".score").textContent = 0
+        document.querySelector("body").style.backgroundColor = "red"
+        //!Oyunu Kaybettiniz
+     }
+
+    }
+
+})
+
+//? Again butonuna basınca ayarlar başlangıç değerlerine kurulsun. Arka plan #2d3436 olsun
+
+document.querySelector(".again").onclick = ()=>{
+    document.querySelector("body").style.backgroundColor = "#2d3436"
+
+    rastgeleSayi = Math.ceil(Math.random()*20)
+    console.log(rastgeleSayi);
+
+    skor = 10
+    document.querySelector(".score").textContent = skor
+
+    document.querySelector(".number").textContent = "?"
+
+    document.querySelector(".guess").value = ""
+
+    mesaj.textContent = "Oyun yeni oyuncu için başlıyor"
+}
+
+
+//! ENTER
+//? Klavyeden enter buttonuna basıldığında check butonunu tetikle.
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Enter"){
+        //*Enter tuşuna basıldığında check butonunu tıkla
+        document.querySelector(".check").click()
+    }
 })
